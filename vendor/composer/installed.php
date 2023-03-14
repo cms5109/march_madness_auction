0773bb4ba964bfc60bff762f690ee1653081c074
Conal Scanlon <conalscanlon@Conals-MacBook-Air.local>
2023-03-13 22:23:00 -0400

    composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dbe6c8b0f73c1fd87db4a128532ed069aa3f6b7f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dbe6c8b0f73c1fd87db4a128532ed069aa3f6b7f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'platformsh/config-reader' => array(
+            'pretty_version' => '2.4.0',
+            'version' => '2.4.0.0',
+            'reference' => '5511abfdb673ccfcf0eac9eaf65204f73edd90fd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../platformsh/config-reader',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
