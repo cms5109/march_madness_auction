dbe6c8b0f73c1fd87db4a128532ed069aa3f6b7f
Conal Scanlon <conalscanlon@Conals-MacBook-Air.local>
2023-03-13 22:09:07 -0400

    adding db config

--- php/header.php
+++ php/header.php
@@ -30,7 +30,7 @@ $query_builder = TRUE;
 use Platformsh\ConfigReader\Config;
 $config = new Config();
 $credentials = $config->credentials('database');
-$platform_server = $credentials['host'];
+$platform_server = $credentials['hostname'];
 $platform_port = $credentials['port'];
 $platform_path = $credentials['path'];
 $platform_username = $credentials['username'];
